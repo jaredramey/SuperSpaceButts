8d432a893f9bfefe2576419c24688512c7186277
Jared Ramey <jared.ramey@students.aie.edu.au>
2018-01-11 13:53:55 -0800

    Added dropdown for screen resolution. Need to finish functionality.

---
+++ SuperSpaceButts/Assets/Scripts/Level/ScreenController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour
+{
+    [HideInInspector]
+    private int screenResolutionX = 400, screenResolutionY = 300;
+    public int defaultScreenResolution = 1;
+
+    void Start()
+    {
+        Debug.Log(Screen.currentResolution);
+        //if(Screen.currentResolution)
+    }
+
+    public void UpdateScreenResolution(int currentSelection)
+    {
+        //5:4
+        if(currentSelection == 0)
+        {
+            screenResolutionX = 500;
+            screenResolutionY = 400;
+        }
+        //4:3
+        else if (currentSelection == 1)
+        {
+            screenResolutionX = 400;
+            screenResolutionY = 300;
+        }
+        //3:2
+        else if (currentSelection == 2)
+        {
+            screenResolutionX = 300;
+            screenResolutionY = 200;
+        }
+        //16:10
+        else if (currentSelection == 3)
+        {
+            screenResolutionX = 1600;
+            screenResolutionY = 1000;
+        }
+        //16:9
+        else if (currentSelection == 4)
+        {
+            screenResolutionX = 1600;
+            screenResolutionY = 900;
+        }
+
+        //Update Resolution
+        Screen.SetResolution(screenResolutionX, screenResolutionY, Screen.fullScreen);
+    }
+}
